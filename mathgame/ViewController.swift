c51aadad5884b64e983af2b2bf9e5aa7bb014699
lucadaniele <44159214+lucadaniele@users.noreply.github.com>
2018-10-15 11:34:58 +0200

    Initial Commit

---
+++ mathgame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mathgame
+//
+//  Created by luca daniele on 15/10/2018.
+//  Copyright © 2018 Paco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
